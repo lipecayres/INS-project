423af8bd3d81eb0994c162ed54266b19fea555a3
Felipe Cayres <felipescayres@gmail.com>
2024-06-26 16:09:25 -0400

    late first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Home Page Slider
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+  /*  autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    }, */   
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
